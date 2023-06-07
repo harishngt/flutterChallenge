31c0f3fe9ac45d64f37df8d00a041e822d0907a5
harishngt <97893600+harishngt@users.noreply.github.com>
2023-06-07 09:27:51 +0530

    Interface and Input created
    User inputs values are added using textfiled and gender and calculate buttons are added.

--- BMIcalculator/lib/mainscreen.dart
+++ BMIcalculator/lib/mainscreen.dart
@@ -12,6 +12,11 @@ class homepg extends StatefulWidget {
 }
 
 class _homepgState extends State<homepg> {
+  bool buttonindex = false;
+  List<String> chipcontent = [
+    'Male',
+    'Female',
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,6 +26,7 @@ class _homepgState extends State<homepg> {
         backgroundColor: Colors.black,
       ),
       body: Container(
+        padding: EdgeInsets.all(15),
         child: Column(
           children: [
             SizedBox(
@@ -56,9 +62,104 @@ class _homepgState extends State<homepg> {
                 ],
               ),
             ),
-            SizedBox(height: 30),
-            Container(),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                for (int i = 0; i < chipcontent.length; i++)
+                  chips(
+                    id: i,
+                    name: chipcontent[i],
+                  )
+              ],
+            ),
+            SizedBox(
+              height: 70,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      'Age:',
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 80,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      'Weight:',
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 80,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      'Height:',
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 80,
+                ),
+                SizedBox(
+                    width: 400,
+                    child: TextButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.orange),
+                            foregroundColor:
+                                MaterialStateProperty.all(Colors.white)),
+                        child: Text('Calculate')))
+              ],
+            )
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget chips({
+    int id = 0,
+    String name = 'Male',
+  }) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 5),
+      width: 180,
+      child: TextButton(
+        onPressed: () {
+          buttonindex = true;
+        },
+        style: TextButton.styleFrom(
+          foregroundColor: Colors.white,
+          backgroundColor:
+              (buttonindex = true) ? (Colors.purple) : (Colors.pink),
+          padding: EdgeInsets.symmetric(
+            horizontal: 10,
+            vertical: 5,
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+        child: Text(
+          name,
+          style: TextStyle(
+            color: Colors.white,
+          ),
         ),
       ),
     );
